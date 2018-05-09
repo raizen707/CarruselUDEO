2a4262a3c95fbd8ca044ce1bf49ccfe50bded19f
steve <raizen707@gmail.com>
2018-05-08 19:42:19 -0600

    Agregar archivos de proyecto.

---
+++ CarruselUDEO/CarruselUDEO/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CarruselUDEO
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			//MainPage = new CarruselUDEO.MainPage();
+			MainPage = new NavigationPage( new CarruselUDEO.MainPage());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
